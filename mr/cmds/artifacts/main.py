28942a0337cac1d82b2d0bd8c69e796f383a17c8
Fang-Pen Lin <hello@fangpenlin.com>
2025-12-27 12:22:17 -0800

    implement view cmd

--- mr/cmds/artifacts/main.py
+++ mr/cmds/artifacts/main.py
@@ -1,5 +1,10 @@
+import importlib.util
+import os.path
+import pathlib
+
 import click
 
+from ...artifacts.registry import collect
 from ..environment import Environment
 from ..environment import pass_env
 from .cli import cli
@@ -9,5 +14,17 @@ from .cli import cli
 @click.argument("MODULE")
 @pass_env
 def view(env: Environment, module: str):
-    # TODO:
-    pass
+    if module.lower().endswith(".py") and os.path.exists(module):
+        module_path = pathlib.Path(module)
+        module_name = module_path.stem
+        spec = importlib.util.spec_from_file_location(module_name, module_path)
+        if spec is None or spec.loader is None:
+            raise ImportError(
+                f"Could not load spec for module '{module_name}' at: {module_path}"
+            )
+
+        module_obj = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module_obj)
+    else:
+        module_obj = importlib.import_module(module)
+    registry = collect([module_obj])
