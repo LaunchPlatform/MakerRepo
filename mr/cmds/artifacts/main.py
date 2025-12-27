e773417917ccc20c40796c81a1ebc00cf91d2114
Fang-Pen Lin <hello@fangpenlin.com>
2025-12-27 12:37:03 -0800

    extract func

--- mr/cmds/artifacts/main.py
+++ mr/cmds/artifacts/main.py
@@ -1,21 +1,20 @@
 import importlib.util
 import os.path
 import pathlib
+from types import ModuleType
 
 import click
 
 from ...artifacts.registry import collect
+from ...artifacts.registry import Registry
 from ..environment import Environment
 from ..environment import pass_env
 from .cli import cli
 
 
-@cli.command(help="View artifact")
-@click.argument("MODULE")
-@pass_env
-def view(env: Environment, module: str):
-    if module.lower().endswith(".py") and os.path.exists(module):
-        module_path = pathlib.Path(module)
+def load_module(module_spec: str) -> ModuleType:
+    if module_spec.lower().endswith(".py") and os.path.exists(module_spec):
+        module_path = pathlib.Path(module_spec)
         module_name = module_path.stem
         spec = importlib.util.spec_from_file_location(module_name, module_path)
         if spec is None or spec.loader is None:
@@ -25,6 +24,24 @@ def view(env: Environment, module: str):
 
         module_obj = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module_obj)
-    else:
-        module_obj = importlib.import_module(module)
-    registry = collect([module_obj])
+        return module_obj
+    return importlib.import_module(module_spec)
+
+
+def collect_artifacts(module_spec: str) -> Registry:
+    return collect([load_module(module_spec)])
+
+
+@cli.command(help="View artifact")
+@click.argument("MODULE")
+@pass_env
+def view(env: Environment, module: str):
+    registry = collect_artifacts(module)
+
+
+@cli.command(help="Export artifact")
+@click.argument("MODULE")
+@pass_env
+def export(env: Environment, module: str):
+    registry = collect_artifacts(module)
+    # TODO:
