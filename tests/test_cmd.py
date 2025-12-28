6ee345e60eed3d1e40f4faba2e24662392b649ab
Fang-Pen Lin <hello@fangpenlin.com>
2025-12-27 17:10:24 -0800

    More tests

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,15 +1,25 @@
 import pathlib
 
+import pytest
 from click.testing import CliRunner
 
+from .helper import switch_cwd
 from mr.cmds.main import cli
 
 
-def test_view(cli_runner: CliRunner, fixtures_folder: pathlib.Path):
-    result = cli_runner.invoke(
-        cli,
-        ["artifacts", "view", str(fixtures_folder / "example.py")],
-        catch_exceptions=False,
-    )
+@pytest.mark.parametrize(
+    "module",
+    [
+        "examples/main.py",
+        "examples.main",
+    ],
+)
+def test_view(cli_runner: CliRunner, fixtures_folder: pathlib.Path, module: str):
+    with switch_cwd(fixtures_folder):
+        result = cli_runner.invoke(
+            cli,
+            ["artifacts", "view", str(fixtures_folder / module)],
+            catch_exceptions=False,
+        )
     assert result.exit_code == 0
     # TODO: run a local mock websocket server to see if the model is correctly sent
